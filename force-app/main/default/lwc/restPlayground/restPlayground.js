abde43688c1f41da5be9a9aced5ca7ed1b9d6beb
SzymonPakuła <szymonpakula98@gmail.com>
2020-12-22 12:13:12 +0100

    first version of playground component

--- force-app/main/default/lwc/restPlayground/restPlayground.js
+++ force-app/main/default/lwc/restPlayground/restPlayground.js
@@ -1,3 +1,51 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import makeRestCallout from '@salesforce/apex/RestCalloutBuilder.makeRestCallout';
 
-export default class RestPlayground extends LightningElement {}
+const options = [
+    { label: 'GET', value: 'GET' },
+    { label: 'POST', value: 'POST' },
+    { label: 'PUT', value: 'PUT' },
+    { label: 'PATCH', value: 'PATCH' },
+    { label: 'DELETE', value: 'DELETE' }
+];
+
+export default class RestPlayground extends LightningElement {
+    options = options;
+    waitningForResponse = false;
+    requestParams = {
+        endpoint: '/Opportunity/',
+        params: 'No Input Parameters',
+        returnValue: 'void',
+        type: 'GET'
+    };
+    responseBody = 'No Body';
+    responseStatusCode = 'No Status Code';
+
+    @track response;
+    @track error;
+
+    submitDetails() {
+        this.responseBody = this.response.body;
+        this.responseStatusCode = this.response.statusCode;
+    }
+
+    async getResponseData() {
+        this.waitningForResponse = true;
+        console.log('click');
+        console.log('SZPAK  this.requestParams', this.requestParams);
+        await makeRestCallout({
+            endpoint: this.requestParams.endpoint,
+            type: this.requestParams.type,
+            returnVal: this.requestParams.returnValue,
+            params: this.requestParams.params
+        })
+            .then((result) => {
+                this.response = result;
+                this.submitDetails();
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+        this.waitningForResponse = false;
+    }
+}
