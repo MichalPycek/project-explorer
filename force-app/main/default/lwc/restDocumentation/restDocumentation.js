44f2eb0085a6f4faa1e18b4063aa57beae183cc6
Pawel <pawel.kalinow98@gmail.com>
2021-01-05 18:03:55 +0100

    Cleaned up code in documentation

--- force-app/main/default/lwc/restDocumentation/restDocumentation.js
+++ force-app/main/default/lwc/restDocumentation/restDocumentation.js
@@ -4,7 +4,6 @@ const columns = [
     { label: 'Method Type', fieldName: 'type', type: 'text', initialWidth: 100 },
     { label: 'Return', fieldName: 'returnValue', type: 'text', initialWidth: 80 },
     { label: 'Endpoint', fieldName: 'endpoint', type: 'text'}
-
 ];
 
 const METHOD_ANNOTATION = '@http';
@@ -17,12 +16,8 @@ export default class RestDocumentation extends LightningElement {
     jsonSuccessContent = JSON.stringify({ foo: 'sample', bar: 'sample' }, null, 4);
 
     @api handleRowUnselection(){
-        console.log('PAKAL: unselecting data table');
-        //Literally couldn't find any other way to deselect all rows
         this.template.querySelector('lightning-datatable').maxRowSelection=0;
-        this.template.querySelector('lightning-datatable').maxRowSelection=1;
- 
-        
+        this.template.querySelector('lightning-datatable').maxRowSelection=1;     
     }
 
     get selectedClassData() {
@@ -30,6 +25,7 @@ export default class RestDocumentation extends LightningElement {
         if (this.selectedApexClass) {
             const allLines = this.selectedApexClass.body.split('\n');
             let classEndpoint, methodParams, checkMultipleLineParams,methodType, methodDefinition, methodReturnVal;
+            
             allLines.forEach((line) => {
                 if (line.includes(CLASS_ANNOTATION)) {
                     classEndpoint = this.findEndpointInLine(line);
@@ -51,12 +47,10 @@ export default class RestDocumentation extends LightningElement {
                     if(line.indexOf(')') !== -1){
                         checkMultipleLineParams = false;
                     }
-                    methodParams = this.appendParamsFromLine(line, methodParams);
-    
+                    methodParams = this.appendParamsFromLine(line, methodParams);   
                 }
 
                 if (classEndpoint && methodType && checkMultipleLineParams==false) {
-                    console.log('METHOD PARAMS', methodParams);
                     tempDataArray.push({
                         type: methodType.toUpperCase(),
                         methodDef: methodDefinition,
@@ -129,14 +123,11 @@ export default class RestDocumentation extends LightningElement {
             } else {
                 if(currentLine.indexOf(')') !== -1){
                     updatedParams = currentLine.substring(0, currentLine.indexOf(')'));
-
                 } else {
                     updatedParams = currentLine;
                 }
             } 
         }
-
         return currentParams + updatedParams.trim();
     }
-
 }
