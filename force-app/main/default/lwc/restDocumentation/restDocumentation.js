ce9c9250eaede0886f523d8d55d89337dfaa8948
Pawel <pawel.kalinow98@gmail.com>
2021-01-05 17:54:21 +0100

    Functionality that extracts method params from multiple lines

--- force-app/main/default/lwc/restDocumentation/restDocumentation.js
+++ force-app/main/default/lwc/restDocumentation/restDocumentation.js
@@ -1,9 +1,10 @@
 import { LightningElement, api } from 'lwc';
 
 const columns = [
-    { label: 'Method Type', fieldName: 'type', type: 'text', initialWidth: 130 },
-    { label: 'Return', fieldName: 'returnValue', type: 'text', initialWidth: 130 },
-    { label: 'Endpoint', fieldName: 'endpoint', type: 'text' }
+    { label: 'Method Type', fieldName: 'type', type: 'text', initialWidth: 100 },
+    { label: 'Return', fieldName: 'returnValue', type: 'text', initialWidth: 80 },
+    { label: 'Endpoint', fieldName: 'endpoint', type: 'text'}
+
 ];
 
 const METHOD_ANNOTATION = '@http';
@@ -16,18 +17,20 @@ export default class RestDocumentation extends LightningElement {
     jsonSuccessContent = JSON.stringify({ foo: 'sample', bar: 'sample' }, null, 4);
 
     @api handleRowUnselection(){
+        console.log('PAKAL: unselecting data table');
+        //Literally couldn't find any other way to deselect all rows
         this.template.querySelector('lightning-datatable').maxRowSelection=0;
         this.template.querySelector('lightning-datatable').maxRowSelection=1;
+ 
+        
     }
 
     get selectedClassData() {
-        let tempDataArray = [];
+        let tempDataArray = [];  
         if (this.selectedApexClass) {
             const allLines = this.selectedApexClass.body.split('\n');
-            let classEndpoint;
+            let classEndpoint, methodParams, checkMultipleLineParams,methodType, methodDefinition, methodReturnVal;
             allLines.forEach((line) => {
-                let methodType, methodDefinition, methodReturnVal;
-
                 if (line.includes(CLASS_ANNOTATION)) {
                     classEndpoint = this.findEndpointInLine(line);
                 }
@@ -35,17 +38,38 @@ export default class RestDocumentation extends LightningElement {
                     methodType = line.substring(line.indexOf(METHOD_ANNOTATION) + 5, line[-1]);
                     methodDefinition = allLines[allLines.indexOf(line) + 1];
                     methodReturnVal = this.findReturnValInMethodDef(methodDefinition);
+                   
+                    if(methodDefinition.indexOf(')') !== -1){
+                        methodParams = this.findParamsInMethodDef(methodDefinition);
+                        checkMultipleLineParams = false;
+                    } else {
+                        checkMultipleLineParams = true;
+                    }
                 }
 
-                if (classEndpoint && methodType) {
+                if(checkMultipleLineParams){
+                    if(line.indexOf(')') !== -1){
+                        checkMultipleLineParams = false;
+                    }
+                    methodParams = this.appendParamsFromLine(line, methodParams);
+    
+                }
+
+                if (classEndpoint && methodType && checkMultipleLineParams==false) {
+                    console.log('METHOD PARAMS', methodParams);
                     tempDataArray.push({
                         type: methodType.toUpperCase(),
                         methodDef: methodDefinition,
                         returnValue: methodReturnVal,
-                        endpoint: 'services/apexrest' + classEndpoint
+                        endpoint: 'services/apexrest' + classEndpoint,
+                        methodParameters: methodParams
                     });
+                    methodDefinition = undefined;
+                    methodType = undefined;
+                    methodParams = undefined;
                 }
             });
+            
             return tempDataArray;
         }
 
@@ -80,4 +104,39 @@ export default class RestDocumentation extends LightningElement {
         methodReturnVal = methodReturnVal.join().trim();
         return methodReturnVal;
     }
+
+    findParamsInMethodDef(methodDef){
+        let methodParams = methodDef.substring(
+            methodDef.indexOf('(') + 1,
+            methodDef.indexOf(')')
+        );
+        if (methodParams === ' ' || methodParams == null || methodParams === '') {
+            methodParams = 'No specified parameters';
+        }
+        return methodParams;
+    }
+
+    appendParamsFromLine(currentLine, currentParams){
+        let updatedParams = '';
+        if(currentParams === undefined){
+            return updatedParams;
+        } else {
+            if(currentLine.indexOf('(') !== -1){
+                updatedParams = currentLine.substring(
+                    currentLine.indexOf('(') + 1,
+                    currentLine.length
+                );
+            } else {
+                if(currentLine.indexOf(')') !== -1){
+                    updatedParams = currentLine.substring(0, currentLine.indexOf(')'));
+
+                } else {
+                    updatedParams = currentLine;
+                }
+            } 
+        }
+
+        return currentParams + updatedParams.trim();
+    }
+
 }
